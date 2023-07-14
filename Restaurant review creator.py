be1ac00fde6f37c57a8b0875b3e9967d37489509
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2023-07-15 00:01:33 +0600

    Add files via upload

---
+++ Restaurant review creator.py
@@ -0,0 +1,29 @@
+import os
+import openai
+import pyttsx3
+
+
+
+openai.api_key = "your api key"
+model_engine = "text-davinci-003"
+user_input = input("Restaurant Name:")
+p = "Turn a few words into a restaurant review" + user_input
+
+response = openai.Completion.create(
+  model=model_engine,
+  prompt=p,
+  max_tokens=1000,
+  top_p=1,
+  stop=None,
+  temperature=0.5,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+r = response.choices[0].text
+print("Review:"+r)
+
+test_to_speech = pyttsx3.init()
+speech = r
+test_to_speech.say(speech)
+test_to_speech.runAndWait()
